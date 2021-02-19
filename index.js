36dfd5a2464c90a98bec6367a7335572b6de0d0a
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-19 15:30:57 -0800

    included isbn in object output

--- index.js
+++ index.js
@@ -2,39 +2,24 @@ const data = require('./data.json')
 
 function transformData(data) {
 
+    console.log(Object.keys(data)[0])
+
     let newArr = []
+    let count = 0
 
     Object.values(data).forEach(function (key) {
+
         let tempObj = { name: '', books: [], isbn: 1234567891234, text: [] }
 
         tempObj.name = key[0]
         tempObj.books = [key[1]]
-        // console.log(key[1])
+        tempObj.isbn = Object.keys(data)[count]
+        // console.log(key[data])
         newArr.push(tempObj)
-
-        // let newObj = {}
-        // newObj[key[0]] = true
-        // newArr.push(newObj)
+        count++
     })
 
     console.log(newArr)
-
-    // console.log(Object.keys(data))
-
-    // Object.keys(data).forEach(function (key) {
-    //     console.log(key)
-    //     key = 
-    // })
-
-    // for (let el of newArr) {
-    //     Object.values(el).forEach(function (key) {
-    //     })
-    //     // console.log(Object.values(el))
-    //     // let newObj2 = {}
-
-    // }
-
-    // console.log(Object.keys(data))
    
 }
 
