e7496ce26b6f552b31c888159cfecda4f9af5248
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-19 14:17:10 -0800

    updated input data to be reflective of problem statement as single object

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const data = require('./data.json')
 
 function transformData(data) {
-    console.log(data)
+
+    console.log(Object.keys(data))
+
+    // let obj = {}
+    // for (let el in data) {
+
+    // }
+
+
+    // console.log(data)
 }
 
 transformData(data)
