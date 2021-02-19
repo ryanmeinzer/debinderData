67dd95ed2feb62a6d5bf7712d4df94bd7bc024f8
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-19 15:34:30 -0800

    included first part of text for each object in main output object

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const data = require('./data.json')
 
 function transformData(data) {
 
-    console.log(Object.keys(data)[0])
+    // console.log(Object.keys(data)[0])
 
     let newArr = []
     let count = 0
@@ -14,7 +14,8 @@ function transformData(data) {
         tempObj.name = key[0]
         tempObj.books = [key[1]]
         tempObj.isbn = Object.keys(data)[count]
-        // console.log(key[data])
+        tempObj.text = [key[2]]
+        // console.log([key[2]])
         newArr.push(tempObj)
         count++
     })
