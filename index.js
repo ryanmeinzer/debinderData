79d2280e39b2425bfdfe4a18f3ab73e7e590384d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-19 16:05:37 -0800

    solved

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const data = require('./data.json')
 
 function transformData(data) {
 
-    // console.log(Object.keys(data)[0])
-
     let newArr = []
     let count = 0
 
@@ -12,17 +10,22 @@ function transformData(data) {
         let tempObj = { name: '', books: [{ title: '', isbn: 1234567891234, text: [] }] }
 
         tempObj.name = key[0]
-        tempObj.books.title = [key[1]]
-        tempObj.books.isbn = Object.keys(data)[count]
-        tempObj.books.text = []
+        console.log(tempObj.books[0].title)
+        tempObj.books[0].title = [key[1]]
+        tempObj.books[0].isbn = Object.keys(data)[count]
+        tempObj.books[0].text = []
         for (i = 2; i < key.length; i++) {
-            tempObj.books.text.push(key[i])
+            tempObj.books[0].text.push(key[i])
         }
         newArr.push(tempObj)
         count++
     })
 
-    console.log(newArr)
+    let jsonStringOutput = JSON.stringify(newArr)
+    let objectOutput = JSON.parse(jsonStringOutput)
+    
+    console.log(jsonStringOutput)
+    console.log(objectOutput)
    
 }
 
